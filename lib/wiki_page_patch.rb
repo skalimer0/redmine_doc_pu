596cca3619b1dc18e3bfc344f08d0075e1f9065c
Tommi Rintala <tommi.rintala@delektre.fi>
2016-04-01 16:00:33 +0300

    indentation fix

--- lib/wiki_page_patch.rb
+++ lib/wiki_page_patch.rb
@@ -2,13 +2,13 @@ require_dependency 'wiki_page'
 
 # Patches Redmine's WikiPage dynamically. Adds a relationship
 module WikiPagePatch
-	def self.included(base)
-		# Same as typing in the class
-		base.class_eval do
-			unloadable # Send unloadable so it will not be unloaded in development
-			has_many :doc_pu_wiki_pages, :dependent => :destroy
-		end
-	end
+  def self.included(base)
+    # Same as typing in the class
+    base.class_eval do
+      unloadable # Send unloadable so it will not be unloaded in development
+      has_many :doc_pu_wiki_pages, :dependent => :destroy
+    end
+  end
 end
 
 # Add module to WikiPage
