f0f4fc5d3f219f21a2afa16218b4dd6612cd75dc
Tommi Rintala <tommi.rintala@delektre.fi>
2016-04-01 15:57:36 +0300

    indentation fix

--- lib/doc_pu_templates.rb
+++ lib/doc_pu_templates.rb
@@ -2,31 +2,31 @@ require 'latex_template'
 
 class DocPuTemplates < LatexTemplate
 	
-	attr_accessor :template_dir
+  attr_accessor :template_dir
 	
-	def initialize(template_dir = '.')
-		@template_dir = Dir.new(template_dir).path
-	end
+  def initialize(template_dir = '.')
+    @template_dir = Dir.new(template_dir).path
+  end
 	
-	def header(filename)
-		self.load(filename)
-		super()
-	end
+  def header(filename)
+    self.load(filename)
+    super()
+  end
 	
-	def load(filename)
-		super(@template_dir + '/' + filename)
-	end
+  def load(filename)
+    super(@template_dir + '/' + filename)
+  end
 	
-	def list()
-		old_work_dir = Dir.pwd
-		# Change to working directory
-		Dir.chdir(@template_dir)
-		
-		# List directory
-		list = Dir.glob('*.tex')
-		
-		# Restore working directory
-		Dir.chdir(old_work_dir)
-		list
-	end
+  def list()
+    old_work_dir = Dir.pwd
+    # Change to working directory
+    Dir.chdir(@template_dir)
+    
+    # List directory
+    list = Dir.glob('*.tex')
+    
+    # Restore working directory
+    Dir.chdir(old_work_dir)
+    list
+  end
 end
